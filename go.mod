1bae15ea7a3f84410f189e407baf2439946f5ffc
Shayan <shayan.fakhtei@gmail.com>
2026-07-23 18:52:14 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/yanando/subbacultcha-monitor
+
+go 1.26.5
+
+require (
+	github.com/PuerkitoBio/goquery v1.12.0 // indirect
+	github.com/andybalholm/cascadia v1.3.4 // indirect
+	github.com/gtuk/discordwebhook v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.15 // indirect
+	github.com/mattn/go-isatty v0.0.23 // indirect
+	github.com/rs/zerolog v1.35.1 // indirect
+	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gtuk/discordwebhook"
+	"github.com/rs/zerolog"
+)
+
+var webhookURL = os.Getenv("DISCORD_WEBHOOK")
+
+const mainLink = "https://subbacultcha.nl/events/"
+
+type Event struct {
+	Title     string
+	Date      string
+	Thumbnail string
+	Location  string
+	URL       string
+}
+
+type Monitor struct {
+	cache  map[string]Event
+	logger zerolog.Logger
+}
+
+func (m *Monitor) Refresh(link string, init bool) {
+	resp, err := http.Get(link)
+
+	if err != nil {
+		m.logger.Err(err).Msg("getting events page")
+	} else if resp.StatusCode > 200 {
+		m.logger.Error().Int("status code", resp.StatusCode)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		m.logger.Err(err).Msg("parsing events page")
+	}
+
+	// iterate over cards
+	doc.Find(".col-xs-12.col-sm-6.col-md-3.card.events-card").Each(func(i int, s *goquery.Selection) {
+		title, err := s.Find(".info").Children().First().Children().First().Html()
+
+		if err != nil {
+			m.logger.Err(err).Msg("error getting title innerhtml")
+		} else if _, exists := m.cache[title]; exists {
+			// already in cache, skip
+		}
+
+		// listing is not in cache, gather extra info and log
+
+		date, err := s.Find(".date").First().Html()
+
+		if err != nil {
+			m.logger.Err(err).Msg("error getting date innerhtml")
+		}
+
+		location, err := s.Find(".location").First().Html()
+
+		if err != nil {
+			m.logger.Err(err).Msg("error getting location innerhtml")
+		}
+
+		thumbnail, exists := s.Find("img").First().Attr("src")
+
+		if !exists {
+			m.logger.Error().Msg("image doesn't have source")
+		}
+
+		url, exists := s.Find("a").First().Attr("href")
+
+		if !exists {
+			m.logger.Err(err).Msg("error getting url")
+		}
+
+		e := Event{
+			title,
+			date,
+			thumbnail,
+			location,
+			url,
+		}
+
+		if !init {
+			m.logger.Info().Any("event", e).Msg("new event found!")
+		}
+
+		m.cache[title] = e
+		if !init {
+			m.Alert(e)
+		}
+	})
+
+	// check for pagination
+
+	loadMore := doc.Find(".next")
+
+	if loadMore.Length() > 0 {
+		nextPageLink, exists := loadMore.First().Attr("href")
+
+		if !exists {
+			m.logger.Error().Msg("nextPagelink doesnt have attr href")
+		}
+
+		m.Refresh(nextPageLink, init)
+	}
+}
+
+func (m *Monitor) Alert(e Event) {
+	embed := discordwebhook.Embed{
+		Title: &e.Title,
+		Url:   &e.URL,
+		Fields: &[]discordwebhook.Field{
+			{
+				Name:   ptr("Date"),
+				Value:  &e.Date,
+				Inline: ptr(true),
+			},
+			{
+				Name:   ptr("Location"),
+				Value:  &e.Location,
+				Inline: ptr(true),
+			},
+		},
+		Thumbnail: &discordwebhook.Thumbnail{
+			Url: &e.Thumbnail,
+		},
+	}
+
+	discordwebhook.SendMessage(webhookURL, discordwebhook.Message{Username: ptr("Subba monitor"), Embeds: &[]discordwebhook.Embed{embed}})
+}
+
+func NewMonitor() *Monitor {
+	output := zerolog.ConsoleWriter{Out: os.Stdout}
+	logger := zerolog.New(output).With().Timestamp().Logger()
+	return &Monitor{
+		cache:  map[string]Event{},
+		logger: logger,
+	}
+}
+
+func main() {
+	if webhookURL == "" {
+		log.Fatal("No DISCORD_WEBHOOK environment variable found")
+	}
+
+	mon := NewMonitor()
+
+	// init
+	mon.Refresh(mainLink, true)
+
+	mon.logger.Info().Int("Current event count", len(mon.cache)).Msg("Initialization complete")
+
+	for {
+		// refresh every 5 mins
+		time.Sleep(time.Minute * 5)
+		mon.Refresh(mainLink, false)
+	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
